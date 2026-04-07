affad203cba084bf25df9adc7e1279b0e17c97f3
Israfil <israfilshaikhp047610@gmail.com>
2026-04-07 06:57:12 +0530

    fixed cors for vercel

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+      'http://localhost:3000',
+      'https://food-donation-flax.vercel.app'
+  ],
   credentials: true
 }));
 app.use(express.json());
--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://food-donation-4akm.onrender.com',
+  baseURL: 'https://food-donation-4akm.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
