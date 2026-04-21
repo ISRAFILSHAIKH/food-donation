c46843a81db6ce94bb1ac3f4904e086563b61b5e
Israfil <israfilshaikhp047610@gmail.com>
2026-04-21 21:28:39 +0530

    debug: log mongo uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MongoDB_URI, {
+    console.log('MONGO_URI being used:', process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
